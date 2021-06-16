8ad732dd2f58ab2e18595da3355005a4d39fb891
andrewnova <tech.easy1@outlook.com>
2021-06-16 18:18:01 -0500

    test con

--- src/Test1.java
+++ src/Test1.java
@@ -1,6 +1,7 @@
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("My git Practice");
-        System.out.println("Test Pull");   
+        System.out.println("Test Pull");
+        System.out.println("Test conflict");
     }
 }
