34790fa92d3a9ecc2afd7c9c43f42219a42cc769
andrewnova <tech.easy1@outlook.com>
2021-06-16 18:34:40 -0500

    test pr

--- src/Test1.java
+++ src/Test1.java
@@ -2,6 +2,9 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("My git Practice");
         System.out.println("Test Pull");  
-        System.out.println("Test case 1);  
+        System.out.println("Test case 1");
+        System.out.println("Test PR");
+
+
     }
 }
